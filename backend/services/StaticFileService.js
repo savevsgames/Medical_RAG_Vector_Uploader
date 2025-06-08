439d173d10662ee0a37bcc43d988441f27fe93bb
Greg Barker <126988787+savevsgames@users.noreply.github.com>
2025-06-08 08:08:49 -0600

    Updated StaticFileService.js

--- backend/services/StaticFileService.js
+++ backend/services/StaticFileService.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { errorLogger } from '../agent_utils/errorLogger.js';
+import { errorLogger } from '../agent_utils/shared/logger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
