d2c0043bb146d305d42b409425ffc66df0970d4c
Greg Barker <126988787+savevsgames@users.noreply.github.com>
2025-06-09 06:27:18 -0600

    Updated tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       colors: {
         // Symptom Savior Brand Colors
         'guardian-gold': '#FFD700',
-        'healing-teal': '#0c798c',
+        'healing-teal': '#027d82',
         'sky-blue': '#AEE1F9',
         'deep-midnight': '#0C1A2A',
         'cloud-ivory': '#F9F9F9',
